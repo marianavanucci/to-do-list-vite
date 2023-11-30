0ad75d334f80dd8080d883568c067ac2aa9f49a3
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-29 21:40:35 -0300

    component Todo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+
+import Todo from './components/Todo'
+
 import './App.css'
 
 function App() {
@@ -27,22 +30,13 @@ function App() {
 
   return (
     <div className='app'>
-      <h1>Lista de Tarefas</h1>
-      <div className="todo-list">
-        {todos.map((todo) => (
-          <div className='todo'>
-            <div className="content">
-              <p>{ todo.text }</p>
-              <p className="category">({todo.category})</p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
-        ))}
-      </div>
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <Todo todo={todo} key={0} ></Todo>
+      ))}
     </div>
+  </div>
   )
 }
 
